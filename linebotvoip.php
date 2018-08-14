93bf420ee0f39f665b0478d74e2c1089750f45d0
sakunagithub <41421069+sakunagithub@users.noreply.github.com>
2018-08-14 11:30:51 +0700

    Update linebotvoip.php

--- linebotvoip.php
+++ linebotvoip.php
@@ -330,13 +330,15 @@
     else if($message == "9" || $message == "9."){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "CAT2Call, one-connect, Link, Hosted PBX\nAccess: access01\nIP: 202.129.61.102\nPort: 5060,80\nCon-Current: 2";
+        $arrayPostData['messages'][0]['text'] = "CAT2Call, one-connect, Link, Hosted PBX\nAccess: access01\nIP: 202.129.61.102\nPort: 5060,80\ncon-current: 2";
         $arrayPostData['messages'][1]['type'] = "text";
-        $arrayPostData['messages'][1]['text'] = "CAT2Call, one-connect, Hosted PBX\nAccess: access02_2\nIP: 202.129.61.118\nPort: 5164\nCon-Current: 2";
+        $arrayPostData['messages'][1]['text'] = "CAT2Call, one-connect, Hosted PBX\nAccess: access02_2\nIP: 202.129.61.118\nPort: 5164\ncon-current: 2";
         $arrayPostData['messages'][2]['type'] = "text";
-        $arrayPostData['messages'][2]['text'] = "one-connect\nAccess: access_onu\nIP: 172.27.99.99\nPort: 5060\nCon-Current: 2";
+        $arrayPostData['messages'][2]['text'] = "one-connect\nAccess: access_onu\nIP: 172.27.99.99\nPort: 5060\ncon-current: 2";
         $arrayPostData['messages'][3]['type'] = "text";
-        $arrayPostData['messages'][3]['text'] = "sip-connect, Hosted PBX\nAccess: access02\nIP: 202.129.61.118\nPort: 5060\nCon-Current: 30";
+        $arrayPostData['messages'][3]['text'] = "sip-connect, Hosted PBX\nAccess: access02\nIP: 202.129.61.118\nPort: 5060\ncon-current: 30";
+        $arrayPostData['messages'][4]['type'] = "text";
+        $arrayPostData['messages'][4]['text'] = "sip-connect\nAccess: access02_5\nIP: 202.129.61.118\nPort: 5165\ncon-current: 5";
         replyMsg($arrayHeader,$arrayPostData);
     } 
     
