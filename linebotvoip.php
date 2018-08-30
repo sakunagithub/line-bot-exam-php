eefb6346509f7d43b196844c7030ad3fab8be45e
sakunagithub <41421069+sakunagithub@users.noreply.github.com>
2018-08-30 13:51:20 +0700

    Update linebotvoip.php

--- linebotvoip.php
+++ linebotvoip.php
@@ -346,7 +346,7 @@
     else if($message == "10" || $message == "10." || $message == "add access"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "Add Access\n> en\n> ใส่ password\n> configure terminal\n> session-router\n> local-policy\n> select\n> กด Enter\n> เลือกแถวว่างเช่น 180\n> พิมพ์ 180 กด Enter\n> show\n> done\n> ex\n> ex\n> ex\n> verify-config\n> save-config\n> activate-config\n> ex";
+        $arrayPostData['messages'][0]['text'] = "Add Access\n> en\n> ใส่ password\n> configure terminal\n> session-router\n> local-policy\n> select\n> กด Enter\n> เลือกแถวว่างเช่น 180\n> พิมพ์ 180 กด Enter\n> show\n> from-address (+66..เบอร์เดิมที่มี.. +66..เบอร์เพิ่มใหม่..)\n> done\n> ex\n> ex\n> ex\n> verify-config\n> save-config\n> activate-config\n> ex";
         replyMsg($arrayHeader,$arrayPostData);
     } 
     
